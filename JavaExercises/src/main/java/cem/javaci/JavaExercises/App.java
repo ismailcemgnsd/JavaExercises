1f4eb76ed0ec6287db114cf131346d644e60dfc8
Cem Gunesdogdu <smilecemsoft@gmail.com>
2019-11-16 01:11:08 +0300

    "First Commit"

---
+++ JavaExercises/src/main/java/cem/javaci/JavaExercises/App.java
@@ -0,0 +1,13 @@
+package cem.javaci.JavaExercises;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
